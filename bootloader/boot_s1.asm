ecbc0ecc15340d73122eb095f74fc1155d3d30d3
vtovkach <vadim003600@gmail.com>
2026-01-29 22:17:01 -0600

    Added code to test if a20 is enabled, and print messages indicating if a20 enabled or not.

--- bootloader/boot_s1.asm
+++ bootloader/boot_s1.asm
@@ -26,11 +26,85 @@ verify_boot:
     cld 
     lodsb 
     test al, al 
-    jz done
+    jz check_a20
     mov ah, 0x0E
     mov bh, 0x00 
     int 0x10 
     jmp verify_boot
+
+
+check_a20:
+    pushf 
+    push ds
+    push es
+    push di
+    push si 
+
+    cli 
+
+    xor ax, ax 
+    mov es, ax
+
+    not ax ; ax -> 0xFFFF
+    mov ds, ax 
+
+    mov di, 0x0500
+    mov si, 0x0510
+
+    mov al, byte [es:di]
+    push ax 
+
+    mov al, byte [ds:si]
+    push ax 
+
+    mov byte [es:di], 0x00
+    mov byte [ds:si], 0xff 
+    
+    cmp byte [es:di], 0xff 
+
+    pop ax 
+    mov [ds:si], al 
+
+    pop ax 
+    mov [es:di], al 
+
+    mov ax, 0 
+    je check_a20_exit
+
+    mov ax, 1 
+
+check_a20_exit:
+    pop si 
+    pop di
+    pop es 
+    pop ds
+    popf 
+
+    cmp ax, 1
+    mov si, a20_ne_msg
+    jne a20_not_enabled
+
+    mov si, a20_e_msg
+
+a20_enabled:
+    cld
+    lodsb 
+    test al, al 
+    jz done 
+    mov ah, 0x0e
+    mov bh, 0x00 
+    int 0x10 
+    jmp a20_enabled
+
+a20_not_enabled:
+    cld
+    lodsb 
+    test al, al 
+    jz done 
+    mov ah, 0x0e
+    mov bh, 0x00 
+    int 0x10 
+    jmp a20_not_enabled
 
 done: 
     jmp $
@@ -38,6 +112,10 @@ done:
 ; Message to indicate successful boot 
 success_msg:
     db "Boot Success", 0 
+a20_e_msg:
+    db "A20 address line is enabled", 0
+a20_ne_msg:
+    db "A20 is not enabled", 0
 
 ; Boot signature 
 times 510-($-$$) db 0 
