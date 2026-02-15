ccc138df710f8276c51b9a19279de9bd3d6ef419
Pope Vaditto IX <vadim003600@gmail.com>
2026-02-14 23:58:08 -0600

    Added display functions for base10 and base16 integers

---
+++ kernel/drivers/vga/vga_num.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+
+void print_uint32_b10(uint32_t x)
+{
+    if(x < 10)
+    {   
+        char ch = '0' + x % 10;
+        putchar(ch);
+
+        return; 
+    }
+
+    print_uint32_b10(x / 10);
+    char ch = '0' + x % 10; 
+    putchar(ch);
+
+    return;
+}
+
+
+void print_uint64_b16(uint64_t x)
+{
+    static const char* d = "0123456789ABCDEF";
+
+    print("0x");
+
+    for (int i = 15; i >= 0; --i)
+    {
+        uint8_t nib = (x >> (i * 4)) & 0xF;
+        putchar(d[nib]);
+    }
+}
---
+++ kernel/include/drivers/vga/vga_num.h
@@ -0,0 +1,10 @@
+#ifndef VGA_NUM
+#define VGA_NUM
+
+#include <stdint.h>
+
+void print_uint32_b10(uint32_t x);
+
+void print_uint64_b16(uint64_t x);
+
+#endif 
