f9e5f37a5d66b1542d518ec37aa92e6c18362c7b
V <vadim003600@gmail.com>
2026-02-21 23:58:53 -0600

    Set up IDT init file.

---
+++ kernel/arch/x86/idt.c
@@ -0,0 +1,37 @@
+/*
+ * IDTR Structure (32-bit Protected Mode)
+ *
+ *  Bits   Field   Size
+ *  -----------------------
+ *  0–15   Limit   2 bytes
+ * 16–47   Base    4 bytes
+ *
+ *  Total size: 6 bytes
+ */
+
+#include <stdint.h>
+
+#define IDT_BASE        0x0000BE80
+#define IDT_ENTRIES     32 
+#define IDT_DESC_SIZE   8
+
+struct __attribute__((packed)) idtr 
+{
+    uint16_t limit; 
+    uint32_t base; 
+};
+
+int idt_Init()
+{
+    struct idtr idt = {.limit = (IDT_ENTRIES * IDT_DESC_SIZE) - 1, .base = IDT_BASE};
+    
+    asm volatile 
+    (
+        
+        :
+        : "r"(limit)
+    );
+
+
+
+}
